a1458813afe8060ce2b8d1d858e564b366259e31
Gurunandan R. Bhat <nandan@knuth.(none)>
2012-12-15 14:38:31 +0530

    Changed conf to suit local conf

--- traveljadoo/lib/Odyssey/Constants.pm
+++ traveljadoo/lib/Odyssey/Constants.pm
@@ -3,6 +3,6 @@ package Odyssey::Constants;
 use strict;
 use warnings;
 
-our $confdir = '/home/nandan/workspace/TRPlanner/traveljadoo/conf/';
+our $confdir = '/home/nandan/Workspace/TRPlanner/traveljadoo/conf/';
 
 1;
